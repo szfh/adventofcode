9dc5a2e566320d340d4c477431ff2600aa11564b
szfh <52595717+szfh@users.noreply.github.com>
2020-12-11 22:03:54 +0000

    day 6 part 2

--- python/day6.py
+++ python/day6.py
@@ -5,8 +5,6 @@ Created on Sun Dec  6 21:34:53 2020
 @author: samue
 """
 
-import numpy as np
-
 file = "H:\\Projects\\adventofcode\\data\\day6.txt"
 
 with open(file,'r') as f:
@@ -22,12 +20,7 @@ def make_tidy_lines(line):
     return(line_tidy)
 
 def get_letters(line):
-    # letters = []
     letters = ''.join(line)
-    # for word in line:
-    #     for l in word:
-    #         if l in letters:
-    #             letters.append(l)
     return(letters)
 
 data_tidy = make_tidy_data(data_raw)
@@ -38,52 +31,19 @@ length = [len(line) for line in letters]
 total_length = sum(length)
 print('part 1 solution: %d' %(total_length))
 
+def get_line_as_set(line):
+    line_set = line
+    for i in range(len(line)):
+        line_set[i] = set(line[i])
+    return(line_set)
+
 def get_common_letters(line):
-    word0 = line[0]
-    for letter in word0:
-        # for word in line[1:]:
-            if letter in line[1:]:
-                print(letter)
-    #letters in all elements of list
-    # return(line)
+    common_letters = line[0]
+    for i in range(1,len(line)):
+        common_letters = common_letters.intersection(line[i])
+    return(common_letters)
 
-common_letters = [get_common_letters(line) for line in data_tidy[:1]]
-
-# triples_sum = list(filter(sums_to_2020, triples))
-
-# np.concatenate(data_tidy[0])
-# ''.join(data_tidy[0])
-# set(data_tidy[1][:])
-# def get_id(line):
-#     row = \
-#         int(line[0])*64 +\
-#         int(line[1])*32 +\
-#         int(line[2])*16 +\
-#         int(line[3])*8 +\
-#         int(line[4])*4 +\
-#         int(line[5])*2 +\
-#         int(line[6])*1
-    
-#     col = \
-#         int(line[7])*4 +\
-#         int(line[8])*2 +\
-#         int(line[9])*1
-    
-#     # print(row, col)
-    
-#     id = row*8 + col
-#     return(id)
-
-
-# ids = [get_id(line) for line in data_tidy]
-# min_id = min(ids)
-# max_id = max(ids)
-# print('part 1 solution: %d' %(max_id))
-
-# for i in range(min_id,max_id):
-#     if (i > min_id) and (i < max_id):
-#         if not (i in ids):
-#             missing_seat = i
-#             break
-
-# print('part 2 solution: %d' %(missing_seat))
+data_set = [get_line_as_set(line) for line in data_tidy]
+common_letters = [get_common_letters(line) for line in data_set]
+common_letters_sum = sum([len(line) for line in common_letters])
+print('part 2 solution: %d' %(common_letters_sum))
