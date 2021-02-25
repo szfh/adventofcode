8fffdb4ed6a008ba0f14328f83c401fae57c11e4
szfh <52595717+szfh@users.noreply.github.com>
2021-02-25 16:26:38 +0000

    day 13 part 2

--- python/day13.py
+++ python/day13.py
@@ -1,9 +1,16 @@
+import sys
 import os
 
 def make_tidy_data(data_raw):
     data_split = data_raw.splitlines()
     n = int(data_split[0])
-    data = data_split[1].split(",")
+    data_split = data_split[1].split(",")
+    data = []
+    for x in data_split:
+        if x == "x":
+            data.append(-1)
+        else:
+            data.append(int(x))
     return (n, data)
 
 def get_last_departure(n, id):
@@ -14,16 +21,38 @@ def get_last_departure(n, id):
         mult += 1
     return(schedule[-1])
 
+def modular_multiplicative_inverse(a: int, b: int) -> int:
+    return pow(a, -1, b)
+
+def chinese_remainder_theorem(data):
+    product = 1
+    for bus, _ in data:
+        product *= bus
+
+    total = 0
+    for bus, index in data:
+        partial_product = product // bus
+        inverse = modular_multiplicative_inverse(partial_product, bus)
+        total += index * partial_product * inverse
+    departure = total % product
+    return(departure)
+
 def part1(n, data):
-    data = [id for id in data if id != "x"]
+    data = [id for id in data if id != -1]
     last_departure = {}
     for id in data:
         last_departure[id] = get_last_departure(n, int(id))
     time, bus = min(last_departure.values()), min(last_departure, key=last_departure.get)
     return((time-n)*int(bus))
 
+def part2(data):
+    bus_and_index = [(bus, bus-index) for index, bus in enumerate(data) if not bus < 0]
+    departure = chinese_remainder_theorem(bus_and_index)
+    return(departure)
+
 def main():
     file = os.path.abspath("../data/day13.txt")
+    print(sys.version)
 
     with open(file,'r') as f:
         data_raw = f.read()
@@ -31,7 +60,7 @@ def main():
     (n, data) = make_tidy_data(data_raw)
 
     print('part 1 solution: %d' %part1(n, data))
-    # print('part 2 solution: %d' %part2(n, data))
+    print('part 2 solution: %d' %part2(data))
 
 if __name__ == "__main__":
     main()
