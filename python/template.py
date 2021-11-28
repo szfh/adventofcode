dc8b7539096cb582bc331e168c1756237924f8cf
szfh <52595717+szfh@users.noreply.github.com>
2021-11-28 08:41:31 +0000

    add template

---
+++ python/template.py
@@ -0,0 +1,25 @@
+import os
+
+def make_tidy_data(data_raw):
+    data_tidy = data_raw.splitlines()
+    return(data_tidy)
+
+def part1(data):
+    return(1)
+
+def part2(data):
+    return(2)
+
+def main():
+    file = os.path.abspath("../../data/2021/day1.txt")
+
+    with open(file,'r') as f:
+        data_raw = f.read()
+
+    data = make_tidy_data(data_raw)
+
+    print('part 1 solution: %d' %part1(data))
+    print('part 2 solution: %d' %part2(data))
+
+if __name__ == "__main__":
+    main()
