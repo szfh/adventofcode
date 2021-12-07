397eb71fccc66b6b9dfe1c7d6db36438b30835ec
szfh <52595717+szfh@users.noreply.github.com>
2021-12-07 21:58:02 +0000

    add functions file

---
+++ python/scripts/aoc_functions.py
@@ -0,0 +1,5 @@
+# https://blog.keiruaprod.fr/2021/11/23/getting-ready-for-adventofcode-in-python.html
+
+def input_splitlines(data_raw):
+    data_tidy = data_raw.splitlines()
+    return(data_tidy)
