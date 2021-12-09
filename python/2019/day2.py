f9be3ef6ebcf39361079a8dfd1d69b0587bc9f99
szfh <52595717+szfh@users.noreply.github.com>
2021-12-09 21:08:40 +0000

    day 2 part 2

--- python/2019/day2.py
+++ python/2019/day2.py
@@ -1,31 +1,35 @@
 import os
+import itertools
 
 def make_tidy_data(data_raw):
     data_split = data_raw.split(",")
     data_tidy = [int(i) for i in data_split]
     return(data_tidy)
 
-def part1(data):
+def run_computer(data,new1,new2):
     data_new = data.copy()
-    data_new[1] = 12
-    data_new[2] = 2
-    i=0
+    data_new[1] = new1
+    data_new[2] = new2
+    i = 0
     while True:
         if data_new[i] == 1:
-            data_new[data_new[i+3]] = data_new[data_new[i+1]] + data_new[data_new[i+2]]
-            i+=4
+            data_new[data_new[i + 3]] = data_new[data_new[i + 1]] + data_new[data_new[i + 2]]
+            i += 4
         elif data_new[i] == 2:
-            data_new[data_new[i + 3]] = data_new[data_new[i+1]] * data_new[data_new[i+2]]
+            data_new[data_new[i + 3]] = data_new[data_new[i + 1]] * data_new[data_new[i + 2]]
             i += 4
         elif data_new[i] == 99:
-            print("99")
-            break
+            return(data_new[0])
         else:
             print("shouldn't be here")
-    return(data_new[0])
+
+def part1(data):
+    return(run_computer(data,12,2))
 
 def part2(data):
-    return(2)
+    for new1, new2 in itertools.product(range(100),range(100)):
+        if run_computer(data,new1,new2) == 19690720:
+            return((100*new1)+new2)
 
 def main():
     file = os.path.abspath("../../data/2019/day2.txt")
