15786da1d6bdf0471c4d86a0b6faabacb1990768
szfh <52595717+szfh@users.noreply.github.com>
2022-11-30 16:57:06 +0000

    day 1 part 1

---
+++ python/2021/day1.py
@@ -0,0 +1,30 @@
+import sys
+import os
+sys.path.append(os.path.abspath("../scripts"))
+from aoc_functions import *
+
+def make_tidy_data(data_raw):
+    data_tidy = data_raw.splitlines()
+    data_numeric = [int(n) for n in data_tidy]
+    return(data_numeric)
+
+def part1(data):
+    count = sum(y>x for x,y in zip(data, data[1:]))
+    return(count)
+
+def part2(data):
+    return(2)
+
+def main():
+    file = os.path.abspath("../../data/2021/day1.txt")
+
+    with open(file,'r') as f:
+        data_raw = f.read()
+
+    data = make_tidy_data(data_raw)
+
+    print('part 1 solution: %d' %part1(data))
+    print('part 2 solution: %d' %part2(data))
+
+if __name__ == "__main__":
+    main()
