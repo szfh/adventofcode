26e320e2b1e16e238c4ef44b65cddb77531dfcbf
szfh <52595717+szfh@users.noreply.github.com>
2022-11-30 19:01:55 +0000

    day 2 part 1

---
+++ python/2021/day2.py
@@ -0,0 +1,40 @@
+import sys
+import os
+sys.path.append(os.path.abspath("../scripts"))
+from aoc_functions import *
+
+def make_tidy_data(data_raw):
+    data_split = data_raw.splitlines()
+    data_tidy = [line.split(' ') for line in data_split]
+    data_tidy = [(x[0], int(x[1])) for x in data_tidy]
+    return(data_tidy)
+
+def part1(data):
+    h,d = 0,0
+    for dir, n in data:
+        match dir:
+            case 'forward':
+                h+=n
+            case 'backward':
+                h-=n
+            case 'up':
+                d-=n
+            case 'down':
+                d+=n
+    return(h*d)
+
+def part2(data):
+    return(2)
+
+def main():
+    file = os.path.abspath("../../data/2021/day2.txt")
+
+    with open(file,'r') as f:
+        data_raw = f.read()
+
+    data = make_tidy_data(data_raw)
+    print('part 1 solution: %d' %part1(data))
+    print('part 2 solution: %d' %part2(data))
+
+if __name__ == "__main__":
+    main()
