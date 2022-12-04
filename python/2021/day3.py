37209e7d1f487c1f8fad83886fe556e3dad205c4
szfh <52595717+szfh@users.noreply.github.com>
2022-12-04 15:00:41 +0000

    day 3 part 2

--- python/2021/day3.py
+++ python/2021/day3.py
@@ -7,7 +7,7 @@ from aoc_functions import *
 
 def make_tidy_data(data_raw):
     data_tidy = data_raw.splitlines()
-    return (data_tidy)
+    return data_tidy
 
 
 def count_values(data):
@@ -19,7 +19,7 @@ def count_values(data):
             breakpoint()
 
 
-def find_rates(data):
+def get_gamma_epsilon(data):
     length = len(data[0])
     gamma, epsilon = [], []
     for position in range(length):
@@ -36,7 +36,48 @@ def find_rates(data):
         else:
             gamma.append('1')
             epsilon.append('0')
+    # breakpoint()
     return (gamma, epsilon)
+
+
+def get_oxygen_co2(data):
+    data_oxygen = data
+    for i in range(len(data_oxygen[0])):
+        zeros, ones = 0, 0
+        for j in range(len(data_oxygen)):
+            match data_oxygen[j][i]:
+                case '0':
+                    zeros += 1
+                case '1':
+                    ones += 1
+        if zeros > ones:
+            data_oxygen = list(filter(lambda x: x[i] == '0', data_oxygen))
+        else:
+            data_oxygen = list(filter(lambda x: x[i] == '1', data_oxygen))
+        if len(data_oxygen) == 1:
+            oxygen = []
+            [oxygen.append(x) for x in data_oxygen[0]]
+            break
+
+    data_co2 = data
+    for i in range(len(data_co2[0])):
+        zeros, ones = 0, 0
+        for j in range(len(data_co2)):
+            match data_co2[j][i]:
+                case '0':
+                    zeros += 1
+                case '1':
+                    ones += 1
+        if zeros > ones:
+            data_co2 = list(filter(lambda x: x[i] == '1', data_co2))
+        else:
+            data_co2 = list(filter(lambda x: x[i] == '0', data_co2))
+        if len(data_co2) == 1:
+            co2 = []
+            [co2.append(x) for x in data_co2[0]]
+            break
+
+    return (oxygen, co2)
 
 
 def bin2dec(bin):
@@ -45,17 +86,19 @@ def bin2dec(bin):
     for bit in bin:
         sum += (int(bit) * mult)
         mult *= 2
-    return (sum)
+    return sum
 
 
 def part1(data):
-    gamma, epsilon = find_rates(data)
+    gamma, epsilon = get_gamma_epsilon(data)
     power = bin2dec(gamma) * bin2dec(epsilon)
-    return (power)
+    return power
 
 
 def part2(data):
-    return (2)
+    oxygen, co2 = get_oxygen_co2(data)
+    power = bin2dec(oxygen) * bin2dec(co2)
+    return power
 
 
 def main():
