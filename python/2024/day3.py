9333381226398a38784e4683609a2f2231aad696
szfh <52595717+szfh@users.noreply.github.com>
2024-12-12 19:05:03 +0000

    day 3 part 2

--- python/2024/day3.py
+++ python/2024/day3.py
@@ -12,6 +12,21 @@ def part1(data: str) -> int:
         total += int(a[0])*int(a[1])
     return(total)
 
+def part2(data: str) -> int:
+    commands = re.findall("mul\([0-9]+,[0-9]+\)|do\(\)|don\'t\(\)", data)
+    do = True
+    total = 0
+    for c in commands:
+        if c == "do()":
+            do = True
+        elif c == "don't()":
+            do = False
+        else:
+            a = re.findall("[0-9]+", c)
+            if do:
+                total += int(a[0])*int(a[1])
+    return(total)
+
 def main():
     file = os.path.abspath("./data/2024/day3.txt")
 
@@ -21,6 +36,7 @@ def main():
     data = make_tidy_data(data_raw)
 
     print('part 1 solution: %d' % part1(data))
+    print('part 2 solution: %d' % part2(data))
 
 if __name__ == "__main__":
     main()
